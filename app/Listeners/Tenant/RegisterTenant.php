00155e8851e10d4f4bb3437bbdf3de3c1a930491
Mohamed Ahmed <error17191@gmail.com>
2018-06-04 23:15:35 +0200

    Ep012 Done

--- app/Listeners/Tenant/RegisterTenant.php
+++ app/Listeners/Tenant/RegisterTenant.php
@@ -3,18 +3,21 @@
 namespace App\Listeners\Tenant;
 
 use App\Events\Tenant\TenantIdentified;
+use App\Tenants\Database\DatabaseManager;
 use App\Tenants\Manager;
 
 class RegisterTenant
 {
+    protected $db;
+
     /**
      * Create the event listener.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(DatabaseManager $databaseManager)
     {
-        //
+        $this->db = $databaseManager;
     }
 
     /**
@@ -26,5 +29,7 @@ class RegisterTenant
     public function handle(TenantIdentified $event)
     {
         app(Manager::class)->setTenant($event->tenant);
+
+        $this->db->createConnection($event->tenant);
     }
 }
---
+++ app/Tenants/Database/DatabaseManager.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tenants\Database;
+
+
+use App\Tenants\Models\Tenant;
+
+class DatabaseManager
+{
+    public function createConnection(Tenant $tenant)
+    {
+        config()->set('database.connections.tenant', $this->getTenantConnection($tenant));
+        dd(config('database.connections.tenant'));
+    }
+
+    protected function getTenantConnection($tenant)
+    {
+        return array_merge(config()->get($this->getDefaultConnectionPath())
+            , $tenant->tenantConnection->only('database')
+        );
+    }
+
+    protected function getDefaultConnectionPath()
+    {
+        return 'database.connections.' . config()->get('database.default');
+    }
+}
